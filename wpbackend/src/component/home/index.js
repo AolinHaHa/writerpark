b97167aaba621733ee7f294c68158ee38a929cb6
Aolin Yang <yangal321@gmail.com>
2019-08-07 09:08:10 -0700

    Aug 6 router

---
+++ wpbackend/src/component/home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>WPHomePage</h2>
+      </div>
+    );
+  }
+}
+
+export default Home;
